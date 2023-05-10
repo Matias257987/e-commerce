d08cad726cc31f62bcb5531685678f2df682f4bc
Matias <acostamatias25@gmail.com>
2023-05-09 21:10:24 -0300

    add: renderizacion de navbar y cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
+import NavBar from "./components/NavBar";
+import CardList from "./components/CardList";
+import useCardList from "./hooks/useCardList";
+
 export default function Home() {
+  const { data: vj = [] } = useCardList();
   return (
     <>
-      <h1>Hola</h1>
+      <NavBar /> {/* renderiza la navbar */}
+      <h1>TITULO</h1>
+      <div>
+        <h3>Agregar Nuevo Juego</h3>
+        <input type="text" placeholder="ingrese un titulo..." />
+        <input type="text" placeholder="ingrese una descripción..." />
+        <button>Agregar</button>
+      </div>
+      <div>
+        {/* renderiza las tarjetas */}
+        <CardList title="Videojuegos" data={vj} />
+      </div>
     </>
   );
 }
