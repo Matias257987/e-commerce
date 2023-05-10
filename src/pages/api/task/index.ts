bee437f09593bc5c310c91dff7c8c563dd2b2e8b
Matias <acostamatias25@gmail.com>
2023-05-09 21:08:52 -0300

    add: metodo GET y POST

--- src/pages/api/task/index.ts
+++ src/pages/api/task/index.ts
@@ -1,13 +1,36 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { conn } from "@/src/utils/database";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { method } = req;
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method, body } = req;
 
   switch (method) {
-    case "GET": //hacer una peticion
-      return res.status(200).json("getting task");
-    case "POST": //hacer una creacion
-      return res.status(200).json("creating task");
+    case "GET": //hace una peticion de toda la info de la tabla "juegos" de la base de datos
+      try {
+        const query = "SELECT * FROM juegos";
+        const response = await conn.query(query);
+
+        return res.status(200).json(response.rows);
+      } catch (error: any) {
+        return res.status(405).json({ error: error.message });
+      }
+    case "POST": //hace una creacion en la tabla "juegos" de la base de datos
+      try {
+        const { title, description } = body;
+
+        const query =
+          "INSERT INTO juegos(title, description) VALUES ($1, $2) RETURNING *";
+        const values = [title, description];
+        const response = await conn.query(query, values);
+
+        return res.status(200).json(response.rows[0]);
+      } catch (error: any) {
+        return res.status(405).json({ error: error.message });
+      }
+
     default: //retorna un error controlado
       return res.status(400).json("invalid method");
   }
