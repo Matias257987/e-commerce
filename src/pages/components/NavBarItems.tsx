c1ed86542aa6508b8df773d4008a094e1758c6ec
Matias <acostamatias25@gmail.com>
2023-06-02 20:35:32 -0300

    add: Link con path

--- src/pages/components/NavBarItems.tsx
+++ src/pages/components/NavBarItems.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import Link from "next/link";
 
 interface NavBarItemsProps {
   label: string;
+  path: any;
 }
 
-const NavBarItems: React.FC<NavBarItemsProps> = ({ label }) => {
-  return <div>{label}</div>;
+const NavBarItems: React.FC<NavBarItemsProps> = ({ label, path }) => {
+  return (
+    <Link href={path}>
+      <div>{label}</div>
+    </Link>
+  );
 };
 
 export default NavBarItems;
