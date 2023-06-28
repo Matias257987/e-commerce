ad160fdf330138282bf3761138cf005421a45a15
Matias <acostamatias25@gmail.com>
2023-06-28 10:08:28 -0300

    se corrigieron algunos bugs

--- src/pages/components/Searchbar.tsx
+++ src/pages/components/Searchbar.tsx
@@ -1,10 +1,47 @@
-const Searchbar = () => {
-  return (
-    <div>
-      <input type="text" placeholder="Buscar..." />
-      <button>icon</button>
-    </div>
-  );
-};
+// import axios from "axios";
+// import { useCallback, useState } from "react";
 
-export default Searchbar;
+// const Searchbar = () => {
+//   const [searchGame, setSearchGame] = useState("");
+
+//   const handleInput = (e: any) => {
+//     setSearchGame(e.target.value);
+//   };
+
+//   const handleSubmit = useCallback(
+//     async (e: React.FormEvent) => {
+//       e.preventDefault();
+
+//       if (searchGame) {
+//         try {
+//           const response = await axios.get("/api/games", {
+//             params: { name: searchGame },
+//           });
+//           console.log(response);
+
+//           setSearchGame("");
+//         } catch (error) {
+//           console.log(error + "Hola");
+//           alert("No se encontro el juego");
+//         }
+//       }
+//     },
+//     [searchGame]
+//   );
+
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit}>
+//         <input
+//           type="text"
+//           placeholder="Buscar..."
+//           value={searchGame}
+//           onChange={handleInput}
+//         />
+//         <button type="submit">Buscar</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default Searchbar;
