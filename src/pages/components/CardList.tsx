5a1082cb019d8593d6309849ceb5f7b311461cfe
Matias <acostamatias25@gmail.com>
2023-06-28 10:02:23 -0300

    add: searchBar y metodo de filtrado por titulo

--- src/pages/components/CardList.tsx
+++ src/pages/components/CardList.tsx
@@ -1,22 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Card";
+import axios from "axios";
 
 interface CardListProps {
-  data: Record<string, any>[];
   title: string;
+  data: { id: string; [key: string]: any }[];
 }
 
-const CardList: React.FC<CardListProps> = ({ data, title }) => {
+const CardList: React.FC<CardListProps> = ({ title, data = [] }) => {
+  const [searchGame, setSearchGame] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  console.log(data);
+
+  const handleSearch = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (searchGame) {
+      try {
+        const response = await axios.get("/api/games", {
+          params: { name: searchGame },
+        });
+
+        const searchResults = response.data.filter((vj: any) =>
+          vj.title.toLowerCase().includes(searchGame.toLowerCase())
+        );
+
+        setSearchResults(response.data);
+      } catch (error) {
+        alert("No se encontró el juego");
+      }
+    }
+  };
+
   return (
     <div>
       <div>
         <p>{title}</p>
       </div>
       <div>
-        {data.map((vj) => (
-          <Card key={vj.id} data={vj} />
-        ))}
+        <form onSubmit={handleSearch}>
+          <input
+            type="text"
+            placeholder="Buscar..."
+            value={searchGame}
+            onChange={(e) => setSearchGame(e.target.value)}
+          />
+          <button type="submit">Buscar</button>
+        </form>
       </div>
+      {!searchGame ? (
+        <div>
+          {data.map((vj: any) => (
+            <Card key={vj.id} data={vj} />
+          ))}
+        </div>
+      ) : (
+        <div>
+          {searchResults.map((vj: any) => (
+            <Card key={vj.id} data={vj} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
